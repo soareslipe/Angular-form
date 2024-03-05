3b73a50f0a303a83bf195c299ac42f51d1775eae
Fellipe Azevedo <zuraffa2015@gmail.com>
2024-03-05 19:50:33 -0300

    Custom checkbox has been created with function to add and remove values from formBuilder. Validations fix

--- src/app/form-body/form-body.component.ts
+++ src/app/form-body/form-body.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { InputFilesCustomComponent } from '../input-files-custom/input-files-custom.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-form-body',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, InputFilesCustomComponent],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule,
+     InputFilesCustomComponent, MatCheckboxModule],
   templateUrl: './form-body.component.html',
   styleUrl: './form-body.component.css'
 })
@@ -23,6 +25,7 @@ export class FormBodyComponent {
       city: ['', Validators.required],
       state: ['', Validators.required],
       zip: ['', Validators.required],
+      toppings: [''],
       fileForm: this.formBuilder.array([])
     });
   }
@@ -33,5 +36,20 @@ export class FormBodyComponent {
 
   onFilesChanged(files: File[]) {
     this.formData.setControl('fileForm', this.formBuilder.array(files));
+  }
+
+  addValue($event: any) {
+    const value = $event.source.value;
+    const checked = $event.checked;
+    const checkValues: string[] = this.formData.get('toppings')?.value || []
+
+    if (checked) {
+      checkValues.push(value);
+    } else {
+      const index = checkValues.indexOf(value)
+      checkValues.splice(index, 1)
+    }
+    this.formData.get('toppings')?.setValue(checkValues)
+    console.log(this.formData.get('toppings')?.value)
   }
 }
